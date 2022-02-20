3b3bb4d06fd083de3c970942387edf583ae1b6e3
Brayan Mairena <80299739+brayan-ios@users.noreply.github.com>
2022-02-20 06:38:44 +0100

    Add files via upload

---
+++ examen.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>examen de poo-uml 2022</title>
+</head>
+<body>
+    
+    <h1 > Examen de POO UML Ocotal</h1>
+        <h1 > Ingenieria en sisteman</h1>
+    <form action="index.php" method="post">
+    
+        <P>ingrese su nombre: <input type="texto"name="texto" size=35>  
+    
+    </p>
+        <p>
+        <button style="background-color:red;">
+            <input type="submit" value="enviar"> 
+            
+
+ 
+</form>
+
+
+
+</body>
+</html>
